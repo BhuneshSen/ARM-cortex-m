d24fec0ca5eea095be347c6a420b11b6774785c0
Bhunesh Sen <bhuneshsen761@gmail.com>
2026-04-05 12:32:09 +0530

    startup.s

--- led-blink-no-IDE/startup.s
+++ led-blink-no-IDE/startup.s
@@ -1,15 +1,18 @@
-.syntax unified
+.syntax unified     /* ins. for asmblr to use Unified Assembly Syntax for ARM*/
 .cpu cortex-m4
 .thumb
 
-.global _start
+.global _start      /* Entry Point (rst hndlr) */
+.extern main
 
 .section .isr_vector
-.word 0x20020000
-.word _start
+.word 0x20020000   /* Initial Stack Pointer */
+.word _start       /* Reset Handler */
 
 .section .text
+.thumb_func
 _start:
     bl main
+
 loop:
     b loop
